96bc18253cc0eb1ed865bd98a0ac06b454ae4eb4
OlivierFrancois <olivier.francois09@gmail.com>
2024-02-06 10:06:29 +0100

    init

---
+++ sync_vhost.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Vérifier si le conteneur est déjà en cours d'exécution et le lance si non
+if [ ! "$(docker ps -q -f name=apache_php82)" ]; then
+    docker-compose up -d
+fi
+
+# Définition de la structure de données
+projects=("bbapp:project_name:bbapp:document_root:bbapp/public:project_path:./../www/2023-bbapp"
+  "adoxia_v3:project_name:adoxia_v3:document_root:adoxia_v3/public:project_path:./../www/adoxia_v3")
+
+# Boucle pour parcourir chaque projet
+for project in "${projects[@]}"; do
+    IFS=':' read -r -a project_info <<< "$project"
+    project_name="${project_info[0]}"
+    document_root="${project_info[2]}"
+    project_path="${project_info[4]}"
+    echo "DEBUT DE $project_name"
+
+    # Créer le .conf
+    docker exec apache_php82-webserver cp "/etc/apache2/sites-available/template.conf" "/etc/apache2/sites-available/${project_name}.conf"
+    # Compléter le fichier .conf
+    docker exec apache_php82-webserver sed -i "s/__document_root__/${document_root}/g" "/etc/apache2/sites-available/${project_name}.conf"
+    docker exec apache_php82-webserver sed -i "s/__domain__/${project_name}/g" "/etc/apache2/sites-available/${project_name}.conf"
+    # Activer le site
+    docker exec apache_php82-webserver a2ensite "${project_name}.conf"
+
+    echo "CREATION DU .CONF SSL"
+    # Créer le .conf
+    docker exec apache_php82-webserver cp "/etc/apache2/sites-available/template-ssl.conf" "/etc/apache2/sites-available/${project_name}-ssl.conf"
+    # Compléter le fichier .conf
+    docker exec apache_php82-webserver sed -i "s/__document_root__/${document_root}/g" "/etc/apache2/sites-available/${project_name}-ssl.conf"
+    docker exec apache_php82-webserver sed -i "s/__domain__/${project_name}/g" "/etc/apache2/sites-available/${project_name}-ssl.conf"
+    # Activer le site
+    docker exec apache_php82-webserver a2ensite "${project_name}-ssl.conf"
+
+#    if ! grep -q "${project_name}.docker" /etc/hosts; then
+#        # Ajouter une entrée dans /etc/hosts pour le vhost du projet sur le Mac
+#        echo "127.0.0.1    ${project_name}.docker" | sudo tee -a /etc/hosts > /dev/null
+#    fi
+done
+
+docker exec apache_php82-webserver service apache2 reload
