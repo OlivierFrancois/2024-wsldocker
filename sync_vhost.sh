b6afbafbbee36873b965147678d1d34b85441a23
OlivierFrancois <olivier.francois09@gmail.com>
2024-02-06 10:50:15 +0100

    échappement des path

--- sync_vhost.sh
+++ sync_vhost.sh
@@ -12,24 +12,24 @@ projects=("bbapp:project_name:bbapp:document_root:bbapp/public:project_path:./..
 # Boucle pour parcourir chaque projet
 for project in "${projects[@]}"; do
     IFS=':' read -r -a project_info <<< "$project"
-    project_name="${project_info[0]}"
-    document_root="${project_info[2]}"
-    project_path="${project_info[4]}"
+    project_name="${project_info[2]}"
+    document_root="${project_info[4]}"
+    project_path="${project_info[6]}"
     echo "DEBUT DE $project_name"
 
     # Créer le .conf
     docker exec apache_php82-webserver cp "/etc/apache2/sites-available/template.conf" "/etc/apache2/sites-available/${project_name}.conf"
     # Compléter le fichier .conf
-    docker exec apache_php82-webserver sed -i "s/__document_root__/${document_root}/g" "/etc/apache2/sites-available/${project_name}.conf"
-    docker exec apache_php82-webserver sed -i "s/__domain__/${project_name}/g" "/etc/apache2/sites-available/${project_name}.conf"
+    docker exec apache_php82-webserver sed -i "s@__document_root__@$(printf '%q' "${document_root}")@g" "/etc/apache2/sites-available/$(printf '%q' "${project_name}").conf"
+
+
     # Activer le site
     docker exec apache_php82-webserver a2ensite "${project_name}.conf"
 
-    echo "CREATION DU .CONF SSL"
     # Créer le .conf
     docker exec apache_php82-webserver cp "/etc/apache2/sites-available/template-ssl.conf" "/etc/apache2/sites-available/${project_name}-ssl.conf"
     # Compléter le fichier .conf
-    docker exec apache_php82-webserver sed -i "s/__document_root__/${document_root}/g" "/etc/apache2/sites-available/${project_name}-ssl.conf"
+    docker exec apache_php82-webserver sed -i "s@__document_root__@$(printf '%q' "${document_root}")@g" "/etc/apache2/sites-available/$(printf '%q' "${project_name}")-ssl.conf"
     docker exec apache_php82-webserver sed -i "s/__domain__/${project_name}/g" "/etc/apache2/sites-available/${project_name}-ssl.conf"
     # Activer le site
     docker exec apache_php82-webserver a2ensite "${project_name}-ssl.conf"
